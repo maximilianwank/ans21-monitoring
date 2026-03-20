ab4fac2be4eecd7d189d99f0406759c3bb3a9766
Maximilian Wank <maximilian.wank@gmx.de>
2026-03-20 22:39:41 +0100

    Update version retrieval method in __init__.py to use importlib.metadata

--- ans21_monitoring/__init__.py
+++ ans21_monitoring/__init__.py
@@ -1,2 +1,6 @@
-# ans21_monitoring package
-__version__ = "0.1.0"
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("ans21-monitoring")
+except PackageNotFoundError:
+    __version__ = "unknown"
